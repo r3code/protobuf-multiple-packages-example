29a581b5528ef3e3cbd9d1274ff938b972d0d8fb
r3code <contact@r3code.ru>
2019-11-18 16:07:11 +0300

    chore: add build scripts for linux

---
+++ example2/proto-gen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script must me at the same directory as `example1proto` to compile proto files
+#
+# example2proto here like namespace
+# protoc can not compile in subfolders so do it manually 
+# protoc must see the root package folder `example1proto` to process imports
+# so we push '.' folder here. If this script should be placed in another folder
+# don't forget to change `push .` to `push <path to the root folder of example1proto>`
+# or protoc won't make imports.
+
+pushd .
+protoc -I. --go_out=plugins=grpc:. mycompany/example2proto/users/*.proto
+protoc -I. --go_out=plugins=grpc:. mycompany/example2proto/auth/*.proto
+protoc -I. --go_out=plugins=grpc:. mycompany/example2proto/*.proto
+popd
+
